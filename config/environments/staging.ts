71dd913714555d2727dd8354a09c9c62cbd4ee9f
Luke Rogerson <contact@lukerogerson.me>
2025-08-07 15:42:03 +0100

    Uncommited JSON

--- config/environments/staging.ts
+++ config/environments/staging.ts
@@ -2,4 +2,9 @@ import { RemoteConfigRaw } from '@/remote-config/types/remote-config-types.gen'
 import { DeepPartial } from '../utils/partial'
 
 // Staging configuration overrides - extends base config with environment-specific settings
-export const stagingOverrides: DeepPartial<RemoteConfigRaw> = {}
+export const stagingOverrides: DeepPartial<RemoteConfigRaw> = {
+  globals: {
+    something: 'cool!!!!!!!!!',
+    somethingElse: 'nooooooooo',
+  },
+}
