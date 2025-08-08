61c1878fc75642c31c990158897709efb3ec6f13
Luke Rogerson <contact@lukerogerson.me>
2025-08-08 14:50:40 +0100

    Update JSON 4

--- config/environments/production.ts
+++ config/environments/production.ts
@@ -5,6 +5,6 @@ import { DeepPartial } from '../utils/partial'
 export const productionOverrides: DeepPartial<RemoteConfigRaw> = {
   globals: {
     something: 'something',
-    somethingElse: 'somethingElse',
+    somethingElse: 'somethingElse!@@@',
   },
 }
