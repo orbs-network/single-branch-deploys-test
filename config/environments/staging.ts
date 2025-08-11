418c1ca67af71342d42cec17a13290b024d87f6c
Luke Rogerson <contact@lukerogerson.me>
2025-08-11 17:15:15 +0100

    Pause

--- config/environments/staging.ts
+++ config/environments/staging.ts
@@ -5,6 +5,6 @@ import { DeepPartial } from '../utils/partial'
 export const stagingOverrides: DeepPartial<RemoteConfigRaw> = {
   globals: {
     something: 'cool!!!!!!!!!',
-    somethingElse: '88888888888',
+    somethingElse: '99999999999',
   },
 }
